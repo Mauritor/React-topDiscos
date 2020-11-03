085dae71326a71aa046717cd24a3076f8dd1c5c8
Mauro <mrtelefonia@gmail.com>
2020-11-02 23:29:13 -0300

    consumo api fake para top2020

--- src/components/Top2020.js
+++ src/components/Top2020.js
@@ -1,51 +1,39 @@
-import React, { Fragment } from 'react';
-import Nav from './Nav'
+import React, { Fragment, useState, useEffect } from 'react';
+import Nav from './Nav';
+import ocean from '../dist/ocean-album.jpg'
 
 const Top2020 = () => {
+    const [items, setItems] = useState([]);
+    const getUsers = async () => {
+        const url = 'http://jsonplaceholder.typicode.com/users'
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+        setItems(data);
+    }
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+
     return (
         <Fragment>
             <Nav statusLast={'activo'} />
             <div className="container separador">
-                <div className="row">
-                    <div className="col-sm-12 col-md-4 ">
-                        <div className="card">
-                            <div className="card-header card-header-icon card-header-rose">
-                                <div className="card-icon">
-                                    <i className="material-icons">language</i>
+                <div className="row d-flex justify-content-around">
+                    {items.map(item => (
+                        <div className="col-sm-12 col-md-6 col-lg-3" key={item.id}>
+                            <div className="card mb-3">
+                                <img className="card-img-top imgAlbum" src={ocean} alt="" />
+                                <div className="card-body">
+                                    <h4 className="card-title">{item.username}</h4>
+                                    <p className="card-text">{item.company.name}</p>
+                                    <p className="card-text"><small class="text-muted">{item.phone}</small></p>
                                 </div>
                             </div>
-                            <div className="card-body">
-                                <h4 className="card-title">Here is the Icon</h4>
-                                 The place is close to Barceloneta Beach and bus stop just...
-                            </div>
                         </div>
-                    </div>
-                    <div className="col-sm-12 col-md-4">
-                        <div className="card">
-                            <div className="card-header card-header-icon card-header-rose">
-                                <div className="card-icon">
-                                    <i className="material-icons">language</i>
-                                </div>
-                            </div>
-                            <div className="card-body">
-                                <h4 className="card-title">Here is the Icon</h4>
-                                 The place is close to Barceloneta Beach and bus stop just...
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-12 col-md-4">
-                        <div className="card">
-                            <div className="card-header card-header-icon card-header-rose">
-                                <div className="card-icon">
-                                    <i className="material-icons">language</i>
-                                </div>
-                            </div>
-                            <div className="card-body">
-                                <h4 className="card-title">Here is the Icon</h4>
-                                 The place is close to Barceloneta Beach and bus stop just...
-                            </div>
-                        </div>
-                    </div>
+                    ))}
+
                 </div>
             </div>
         </Fragment>
